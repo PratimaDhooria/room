fcdac5711d8d8e0274b7bf0a7c7c3026e1a3658c
Pratima Dhooria <Pratimadhuriya000@gmail.com>
2026-02-18 19:41:44 +0530

    Refactor SearchRentalProperty component for clarity

--- UI/src/Components/SearchRentalPropertyComponent/SearchRentalProperty.js
+++ UI/src/Components/SearchRentalPropertyComponent/SearchRentalProperty.js
@@ -1,53 +1,50 @@
-import './SearchRentalProperty.css';
-import axios from 'axios';
-import { useState , useEffect } from 'react';
-import { __subcategoryapiurl , __categoryapiurl , __userapiurl } from '../../Api.url';
-import { Link } from 'react-router-dom';
+import "./SearchRentalProperty.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { __categoryapiurl } from "../../Api.url";
+import { Link } from "react-router-dom";
 
 function SearchRentalProperty() {
 
-  const [ cDetails , setCategoryDetails ] = useState([]);
+  const [cDetails, setCategoryDetails] = useState([]);
 
-  useEffect(()=>{
-    axios.get(__categoryapiurl+"fetch").then((response)=>{
-      setCategoryDetails(response.data);
-    }).catch((error)=>{
-      console.log(error);        
-    });        
-  });
+  useEffect(() => {
+    axios
+      .get(__categoryapiurl + "fetch")
+      .then((response) => {
+        setCategoryDetails(response.data || []);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []); // ✅ dependency added
 
   return (
-  <>  
-  <div id="tooplate_content">
+    <div id="tooplate_content">
+      <div className="content_box content_box_last">
+        <h2>Search Rental Property &gt;&gt;</h2>
 
-<div class="content_box content_box_last">
-  <h2>Search Rental Property &gt;&gt;</h2>
-
-  <center>  
-  <div id="catmain" >
-  { 
-    cDetails.map((row)=>(
-      <Link to={`/searchsc/${row.catnm}`} >
-      <div class="catpart" >
-        <img src={`assets/uploads/categoryicons/${row.caticonnm}`} height={120} width={150} />
-        <br/>
-        <b>{row.catnm}</b>
+        <div id="catmain">
+          {cDetails.map((row) => (
+            <Link key={row._id} to={`/searchsc/${row.catnm}`}>
+              <div className="catpart">
+                <img
+                  src={`assets/uploads/categoryicons/${row.caticonnm}`}
+                  height={120}
+                  width={150}
+                  alt={row.catnm}
+                />
+                <br />
+                <b>{row.catnm}</b>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
-      </Link>
-    ))
-  }  
-  </div>    
-  </center>
 
-</div>
-
-<div class="cleaner"></div>
-</div>
-  </>
+      <div className="cleaner"></div>
+    </div>
   );
 }
 
 export default SearchRentalProperty;
-
-
-
