6e979c2ab45b626e9b72a0a4618b1fb40c296c2e
Pratima Dhooria <Pratimadhuriya000@gmail.com>
2026-02-18 19:33:39 +0530

    Refactor Header component for improved accessibility
    Updated image tag to include alt attribute for accessibility.

--- UI/src/Components/HeaderComponent/Header.js
+++ UI/src/Components/HeaderComponent/Header.js
@@ -1,14 +1,17 @@
-import './Header.css';
+import "./Header.css";
 
 function Header() {
   return (
-   <>
-      <div className='container'>
-       {/* <img className='imag' src='./assets/images/logo.webp'></img> */}
-       <img className='imag'src='./assets/images/logo22.jpg'></img>
-       <h1 className='h'> ROOM RENT </h1>
-    </div>
-   </>
+    <>
+      <div className="container">
+        <img
+          className="imag"
+          src="./assets/images/logo22.jpg"
+          alt="Room Rent Logo"
+        />
+        <h1 className="h">ROOM RENT</h1>
+      </div>
+    </>
   );
 }
 
