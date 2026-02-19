8ca6fbf09e8153a11562e4107dbccf383ec58bd1
Pratima Dhooria <Pratimadhuriya000@gmail.com>
2026-02-19 15:35:52 +0530

    Update Verifyuser.js

--- UI/src/Components/VerifyuserComponent/Verifyuser.js
+++ UI/src/Components/VerifyuserComponent/Verifyuser.js
@@ -1,32 +1,47 @@
-import { Navigate , useParams } from 'react-router-dom';
-import { useState , useEffect } from 'react';
-import { __userapiurl } from '../../Api.url';
-import axios from 'axios';
+import { Navigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { __userapiurl } from "../../Api.url";
+import axios from "axios";
 
-function Verifyuser()
-{
-    const params = useParams(); 
+function Verifyuser() {
+  const params = useParams();
+  const [verified, setVerified] = useState(false);
 
-    useEffect(()=>{
+  useEffect(() => {
+    if (!params.vemail) return;
 
-     axios.get(__userapiurl+"fetch",{
-        params :  {"email":params.vemail} 
-    }).then((response)=>{
-        if(response.data[0].__v==0)
-        {
-            var updateDetails={"condition_obj":{"email":params.vemail},"content_obj":{"status":1,"__v":1}}; 
-            axios.patch(__userapiurl+"update",updateDetails).then((response)=>{
-               alert("User verified....");    
-            });    
-        }       
-     });
-    },[]);
+    axios
+      .get(__userapiurl + "fetch", {
+        params: { email: params.vemail },
+      })
+      .then((response) => {
+        const user = response.data?.[0];
 
-    return(
-        <div>
-            <Navigate to='/login' />
-        </div>
-    )
+        if (user && user.__v === 0) {
+          const updateDetails = {
+            condition_obj: { email: params.vemail },
+            content_obj: { status: 1, __v: 1 },
+          };
+
+          axios.patch(__userapiurl + "update", updateDetails).then(() => {
+            alert("User verified successfully!");
+            setVerified(true);
+          });
+        } else {
+          setVerified(true);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        setVerified(true);
+      });
+  }, [params.vemail]);
+
+  if (verified) {
+    return <Navigate to="/login" />;
+  }
+
+  return <div>Verifying user...</div>;
 }
 
 export default Verifyuser;
