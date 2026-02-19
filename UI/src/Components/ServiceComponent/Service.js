4050849cdc56b1f8af413a3f324fd7f8766c56e4
Pratima Dhooria <Pratimadhuriya000@gmail.com>
2026-02-19 15:25:13 +0530

    Update Service.js

--- UI/src/Components/ServiceComponent/Service.js
+++ UI/src/Components/ServiceComponent/Service.js
@@ -2,55 +2,66 @@ import './Service.css';
 
 function Service() {
   return (
-   <>
-     <div className='services'>
+    <>
+      <div className='services'>
         <h1 className='Feature-services'>Featured Services</h1>
         <p className='offer'> Services we Offer</p>
-        <br />
-        <br />
-        <br />
 
         <div className='row'>
-          <div className='col'>
-            <div className='card'>
-              <img src="./assets/images/service1.jpg" alt="Service 1" />
 
-               <div className='card-body'>
-                <h2 className='h2-body'>2BHK Flat for Rent in indore </h2>
-                <p className='p-body'>
-                   Rental 2BHK Apartments in indore : Rent RS 8,999
-                </p>
-                <button className='more'>Learn More</button>
-              </div>
-            </div>
-          </div>
           <div className='col'>
             <div className='card'>
-              <img className='card-img' src='./assets/images/images.jpeg'></img>
+              <img 
+                src="./assets/images/service1.jpg" 
+                alt="2BHK Flat in Indore"
+              />
               <div className='card-body'>
-                <h2 className='h2-body'>2BHK Flat for Rent in indore </h2>
+                <h2 className='h2-body'>2BHK Flat for Rent in Indore</h2>
                 <p className='p-body'>
-                   Rental 2BHK Apartments in indore : Rent RS 8,999
+                  Rental 2BHK Apartments in Indore : Rent RS 8,999
                 </p>
                 <button className='more'>Learn More</button>
               </div>
             </div>
           </div>
+
           <div className='col'>
             <div className='card'>
-              <img className='card-img1' src='./assets/images/images01.jpeg'></img>
+              <img 
+                className='card-img' 
+                src='./assets/images/images.jpeg'
+                alt="Affordable 2BHK Apartment"
+              />
               <div className='card-body'>
-                <h2 className='h2-body'>2BHK Flat for Rent in indore </h2>
+                <h2 className='h2-body'>2BHK Flat for Rent in Indore</h2>
                 <p className='p-body'>
-                   Rental 2BHK Apartments in indore : Rent RS 8,999
+                  Rental 2BHK Apartments in Indore : Rent RS 8,999
                 </p>
                 <button className='more'>Learn More</button>
               </div>
             </div>
           </div>
+
+          <div className='col'>
+            <div className='card'>
+              <img 
+                className='card-img1' 
+                src='./assets/images/images01.jpeg'
+                alt="Premium 2BHK Rental"
+              />
+              <div className='card-body'>
+                <h2 className='h2-body'>2BHK Flat for Rent in Indore</h2>
+                <p className='p-body'>
+                  Rental 2BHK Apartments in Indore : Rent RS 8,999
+                </p>
+                <button className='more'>Learn More</button>
+              </div>
+            </div>
+          </div>
+
         </div>
-     </div>
-   </>
+      </div>
+    </>
   );
 }
 
